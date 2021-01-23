684225e3a73777dfb770db5af247b23888464cc4
bobzera <weslleyzera2020@gmail.com>
2021-01-23 15:58:53 +0000

    first

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 import Nav from '../components/nav'
+import Image from 'next/image'
 
 export default function IndexPage() {
   return (
     <div>
-      <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-          Next.js + Tailwind CSS 2.0
-        </h1>
-      </div>
+      <div className="text-center">
+        <Image width="300" height="300" src="/images/morcego.png"/>
+        <div className="p-4">          
+          <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
+            How to became a Morcego in London and make some money
+          </h1>
+        </div>
+        <Image width="300" height="300" src="/images/2r6D.gif"/>     
+      </div>      
     </div>
   )
 }
