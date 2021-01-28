a7df8b6c9500122927493a70f8c935493d5d1bae
bobzera <weslleyzera2020@gmail.com>
2021-01-28 08:01:20 +0000

    tela morcego

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,18 @@ export default function IndexPage() {
         <Image width="300" height="300" src="/images/morcego.png"/>
         <div className="p-4">          
           <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-            How to became a Morcego in London and make some money
+            How to became a Morcego in London
           </h1>
+          <h2 className="text-3xl text-gray-700 py-4">
+            and make money 
+          </h2>
+          
         </div>
-        <Image width="300" height="300" src="/images/2r6D.gif"/>     
+        <div className="grid gap-4 p-4">
+            <a className="text-lg px-6 py-4 border-2 hover:bg-blue-900 border-blue-700 bg-blue-700 rounded text-white mr-4">What's that?</a>
+            <a className="text-lg px-6 py-4 border-2 hover:bg-gray-700 hover:text-white border-gray-700 rounded mr-4">I'm already a Morcego</a>            
+          </div>
+             
       </div>      
     </div>
   )
---
+++ pages/morcego/[morcego].js
@@ -0,0 +1,10 @@
+export default function Morcego(){
+    return (<>
+     
+        <div>
+            <h1>Fala morcego fulano</h1>
+            <h2>Defina o que e morcego?</h2>
+        </div>   
+    
+    </>)
+}
